15cbde8acaab70eebcc9ed32326d375c26cc674c
MaximeLanca <lanca.maxime.ber@gmail.com>
2025-04-24 17:51:15 +0200

    Update all

--- LITreview/LITrevue/urls.py
+++ LITreview/LITrevue/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from activity.views import create_ticket, create_review, home, display_feed_tickets, display_followers, follow_user, unfollow_user
+from activity.views import create_ticket, create_review, home, display_feed, display_followers, follow_user, unfollow_user,search_user_to_follow,create_ticket_and_review
 from django.conf.urls.static import static
 from django.conf import settings
 from accounts.views import signUp, LoginPage, logout_user
@@ -12,13 +12,14 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('new-ticket/', create_ticket, name='create_ticket'),
     path('new-review/', create_review, name='create_review'),
+    path('new_ticket_and_review/', create_ticket_and_review, name='create_ticket_and_review'),
     path('registration/',signUp, name="signUp"), 
     path('logout/', logout_user, name='logout'),
-    path('feed/', display_feed_tickets, name='feed'),
-    #path('followers/', display_followers, name='followers'),
+    path('feed/', display_feed, name='feed'),
     path('follow/<int:user_id>/', follow_user, name='followers'),
     path('unfollow/<int:user_id>/',unfollow_user, name='unfollow_user'),
     path('followers/', display_followers, name='display_followers'),
+    path('search-follow/', search_user_to_follow, name='search_user_to_follow'),
 ]
 
 if settings.DEBUG:
--- LITreview/activity/views.py
+++ LITreview/activity/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TicketForm, ReviewForm
-from .models import Ticket, UserFollows
+from .models import Ticket, UserFollows, Review
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.db import models
@@ -33,22 +33,58 @@ def create_review(request):
     return render (request, 'new_review.html', context)
 
 @login_required
+def create_ticket_and_review(request):
+    if request.method == 'POST':
+        ticket_form = TicketForm(request.POST, request.FILES)
+        review_form = ReviewForm(request.POST)
+        
+        if ticket_form.is_valid() and review_form.is_valid():
+         
+            ticket = ticket_form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.ticket = ticket
+            review.save()
+            
+            return redirect('home')
+    else:
+        ticket_form = TicketForm()
+        review_form = ReviewForm()
+
+    context = {
+        'ticket_form': ticket_form,
+        'review_form': review_form,
+    }
+    return render(request, 'new_ticket_and_review.html', context)
+
+@login_required
 def home(request):
     #flux = models.objects.filter(contributors__in=request.Ticket.all())
     #context = {'flux' : flux,}
     return render (request, 'home.html')
 
-def display_feed_tickets(request):
-    tickets = Ticket.objects.all()
-    context = {"tickets": tickets}
+def display_feed(request):
+    user = request.user
+    followed_users = user.followed_users.values_list('followed_id', flat=True)
+    all_connected_users = list(followed_users) + [user.id]
+
+    tickets = Ticket.objects.filter(user_id__in=all_connected_users)
+    reviews = Review.objects.filter(user_id__in=all_connected_users)
+
+    feed_items = list(tickets) + list (reviews)
+    feed_items.sort(key=lambda item: item.time_created, reverse=True)
+    context = {'feed_items': feed_items}
     return render(request,'feed.html',context)
 
 @login_required
 def display_followers(request):
+    User = get_user_model()
     followed_users = UserFollows.objects.filter(follower=request.user)
     all_users = User.objects.exclude(id=request.user.id)
     not_followed_users = all_users.exclude(id__in=[user.followed.id for user in followed_users])
-
     context = {
         "followed_users": followed_users,
         "not_followed_users": not_followed_users,
@@ -56,10 +92,10 @@ def display_followers(request):
     }
     return render(request, 'followers.html', context)
 
-User = get_user_model()
 
 @login_required
 def follow_user(request, user_id):
+    User = get_user_model()
     user_to_follow = get_object_or_404(User, id=user_id)
     if not UserFollows.objects.filter(follower=request.user, followed=user_to_follow).exists():
         UserFollows.objects.create(follower=request.user, followed=user_to_follow)
@@ -67,6 +103,20 @@ def follow_user(request, user_id):
 
 @login_required
 def unfollow_user(request, user_id):
+    User = get_user_model()
     user_to_unfollow = get_object_or_404(User, id=user_id)
     UserFollows.objects.filter(follower=request.user, followed=user_to_unfollow).delete()
+    return redirect('display_followers')
+
+@login_required
+def search_user_to_follow(request):
+    User = get_user_model()
+    if request.method == "POST":
+        username = request.POST.get("username")
+        try:
+            user_to_follow = User.objects.get(username=username)
+            if user_to_follow != request.user and not UserFollows.objects.filter(follower=request.user, followed=user_to_follow).exists():
+                UserFollows.objects.create(follower=request.user, followed=user_to_follow)
+        except User.DoesNotExist:
+            pass
     return redirect('display_followers')
